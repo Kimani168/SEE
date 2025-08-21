c2edefb9d905778e8834548ecce4cc5f08e4b187
Kimani168 <kimani23.gabriel@student.cuk.ac.ke>
2025-08-21 08:28:21 +0300

    Create basic_calculator.py

---
+++ basic_calculator.py
@@ -0,0 +1,19 @@
+num1 = float(input("Enter the first number: "))
+
+operation = input("Enter the operation (+, -, *, /): ")
+
+num2 = float(input("Enter the second number: "))
+
+if op == '+':
+    print(num1 + num2)
+elif op == '-':
+    print(num1 - num2)
+elif op == '*':
+    print(num1 * num2)
+elif op == '/':
+    if num2 != 0
+        print(num1 / num2)
+    else:
+        print("Error: Cannot divide by zero.")
+else:
+    print("Invalid Operation Used!")
